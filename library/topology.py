8af345f789982215d607174a6be0164284498e56
Egas Santander Daniela <daniela.egassantander@epfl.ch>
2021-11-18 16:44:59 +0100

    changing to pyflagsercontain for node_participation

--- library/topology.py
+++ library/topology.py
@@ -26,12 +26,14 @@ def node_participation(adj, neuron_properties):
     # Input: adj adjancency matrix representing a graph with 0 in the diagonal, neuron_properties as data frame with index gid of nodes
     # Out: List L of lenght adj.shape[0] where L[i] is a list of the participation of vertex i in simplices of a given dimensio
     # TODO:  Change pyflagsercontain so that it takes as input sparse matrices
-    from pyflagsercontain import flagser_count
+    import pyflagsercount
     import pandas as pd
     adj = adj.astype('bool').astype('int')  # Needed in case adj is not a 0,1 matrix
-    par = flagser_count(adj.toarray())
+    par=pyflagsercount.flagser_count(M,containment="delete_me")['contain_counts'][0]
     par = {i: par[i] for i in np.arange(len(par))}
-    par=pd.DataFrame.from_dict(par, orient="index").fillna(0)
+    par=pd.DataFrame.from_dict(par, orient="index").fillna(0).astype(int)
     par=par.join(ninfo['gid'])
     return par
+
+
 

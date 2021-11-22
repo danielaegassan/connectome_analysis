dfa8ccbade9046bbd0fcab54024c706cb05d4194
Egas Santander Daniela <daniela.egassantander@epfl.ch>
2021-11-22 16:52:39 +0100

    Updating betti_counts to allow for different approximation values

--- library/topology.py
+++ library/topology.py
@@ -11,15 +11,54 @@ def simplex_counts(adj, neuron_properties=[]):
     return flagser_count_unweighted(adj, directed=True)
 
 def betti_counts(adj, neuron_properties=[], min_dim=0, max_dim=[], directed=True, coeff=2, approximation=None):
-    #Compute betti counts of adj which represents a direted graph parameters as in pyflagsercount
-    #Delete neuron properties from input?
     from pyflagser import flagser_unweighted
     import numpy as np
+    adj=adj.astype('bool').astype('int') #Needed in case adj is not a 0,1 matrix
     if max_dim==[]:
         max_dim=np.inf
-    adj=adj.astype('bool').astype('int') #Needed in case adj is not a 0,1 matrix
-    return flagser_unweighted(adj, min_dimension=min_dim,
-                                    max_dimension=max_dim, directed=True, coeff=2, approximation=None)['betti']
+
+    if approximation==None:
+        print("Run without approximation")
+        return flagser_unweighted(adj, min_dimension=min_dim,
+                                  max_dimension=max_dim, directed=True, coeff=2, approximation=None)['betti']
+    else:
+        assert (all([isinstance(item,int) for item in approximation])) # asssert it's a list of integers
+        approximation=np.array(approximation)
+        bettis=[]
+
+        #Make approximation vector to be of size max_dim
+        if max_dim!=np.inf:
+            if approximation.size-1 < max_dim:#Vector too short so pad with -1's
+                approximation=np.pad(approximation,(0,max_dim-(approximation.size-1)),'constant',constant_values=-1)
+            if approximation.size-1>max_dim:#Vector too long, select relevant slice
+                approximation=approximation[0:max_dim+1]
+            #Sanity check
+            print("Correct dimensions for approximation:", approximation.size==max_dim+1)
+
+        #Split approximation into sub-vectors of same value to speed up computation
+        diff=approximation[1:]-approximation[:-1]
+        slice_indx=np.array(np.where(diff!=0)[0])+1
+
+        #Compute betti counts
+        for dims_range in  np.split(np.arange(approximation.size),slice_indx):
+            n=dims_range[0] #min dim for computation
+            N=dims_range[-1] #max dim for computation
+            a=approximation[n]
+            if a==-1:
+                a=None
+            print("Run betti for dim range {0}-{1} with approximation {2}".format(n,N,a))
+            bettis=bettis+flagser_unweighted(adj, min_dimension=n,
+                                        max_dimension=N, directed=True, coeff=2, approximation=a)['betti']
+
+        if max_dim==np.inf:
+            n=approximation.size #min dim for computation
+            N=np.inf #max dim for computation
+            a=None
+            print("Run betti for dim range {0}-{1} with approximation {2}".format(n,N,a))
+            bettis=bettis+flagser_unweighted(adj, min_dimension=n,
+                                        max_dimension=N, directed=True, coeff=2, approximation=a)['betti']
+
+        return bettis
 
 def node_participation(adj, neuron_properties):
     # Compute the number of simplices a vertex is part of

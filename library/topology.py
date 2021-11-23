820901c397cc5c3b45ce2c0631eb964edabec45f
matsantoro <matteosantoro561@gmail.com>
2021-11-23 12:58:54 +0100

    Initial whole simplex count

--- library/topology.py
+++ library/topology.py
@@ -1,6 +1,11 @@
+import scipy.sparse as sp
+import numpy as np
+import pyflagsercount as pfc
+
+from pathlib import Path
+from tqdm import tqdm
 # Functions that take as input a (weighted) network and give as output a topological feature.
 #TODO: rc_in_simplex, filtered_simplex_counts, persitence
-
 
 def simplex_counts(adj, neuron_properties=[]):
     #Compute simplex counts of adj
@@ -75,5 +80,66 @@ def node_participation(adj, neuron_properties):
     return par
 
 
+#INPUT: Address of binary file storing simplices
+#OUTPUT: A list if lists L where L[i] contains the vertex ids of the i'th simplex,
+#          note the simplices appear in no particular order
+def binary2simplex(address):
+    X = np.fromfile(address, dtype='uint64')                         #Load binary file
+    S=[]                                                             #Initialise empty list for simplices
 
+    i=0
+    while i < len(X):
+        b = format(X[i], '064b')                                     #Load the 64bit integer as a binary string
+        if b[0] == '0':                                              #If the first bit is 0 this is the start of a new simplex
+            S.append([])
+        t=[int(b[-21:],2), int(b[-42:-21],2), int(b[-63:-42],2)]     #Compute the 21bit ints stored in this 64bit int:
+        for j in t:
+            if j != 2097151:                                         #If an int is 2^21 this means we have reached the end of the simplex, so don't add it
+                S[-1].append(j)
+        i+=1
+    return S
 
+def simplex_matrix(adj: sp.csc_matrix, temp_folder: Path, verbose: bool = False) -> np.array:
+    """
+    Returns the list of simplices in matrix form for storage. The matrix is
+    a n_simplices x max_dim matrix, where n_simplices is the total number of simplices
+    and max_dim is the maximum encountered simplex dimension.
+    
+    :param adj: Sparse csc matrix to compute the simplex list of.
+    :type: sp.scs_matrix
+    :param temp_folder: Relative path where to store the temporary flagser files to.
+    :type: Path
+    :param verbose: Whether to have the function print steps.
+    :type: bool
+
+    :return m: Matrix containing the simplices. The first row will be the indices at which each
+    dimension ends. Successive rows will store simplices from each dimension.
+    :rtype: np.array
+    """
+    def vmessage(message):
+        if verbose:
+            print(message)
+    temp_folder.mkdir(exist_ok = True, parents=True)
+    for path in temp_folder.glob("*"):
+        raise FileExistsError("Found file in " + str(temp_folder.absolute()) + ". Aborting.")
+    vmessage("Flagser count started execution")
+    counts = pfc.flagser_count(adj, binary=str(temp_folder / "temp"), min_dim_print=1, threads = 1)
+    vmessage("Flagser count completed execution")
+    mdim = len(counts['cell_counts'])
+    simplex_matrix = np.zeros((np.sum(counts['cell_counts'][1:]) + 1, mdim), dtype=np.int32)
+    simplex_matrix[0,:] = counts['cell_counts'] #Use this both for future reference and for positioning simplices
+    simplex_matrix[0,0] = 0
+    simplex_matrix[0] = np.cumsum(simplex_matrix[0])
+    vmessage("Parsing flagser output")
+    for path in temp_folder.glob("*"):
+        vmessage("Parsing " + str(path))
+        simplex_list = binary2simplex(path)
+        if verbose:
+            simplex_list = tqdm(simplex_list, desc="Parsed simplices", total = simplex_matrix[0,-1])
+        for simplex in simplex_list:
+             simplex_matrix[simplex_matrix[0,len(simplex)-2] + 1, :len(simplex)] = simplex
+             simplex_matrix[0, len(simplex)-2]+=1
+    vmessage("Generated simplex matrix")
+    np.save(temp_folder/"matrix.npy", simplex_matrix)
+    vmessage("Saved simplex matrix")
+    return simplex_matrix

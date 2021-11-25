f5ace3f49c3dd308c40df19e7330f74b40b87724
Christoph Pokorny <christoph.pokorny@epfl.ch>
2021-11-25 17:47:13 +0100

    Added 3rd order model

--- library/modelling.py
+++ library/modelling.py
@@ -17,6 +17,7 @@ import pickle
 PROB_CMAP = plt.cm.get_cmap('hot')
 DATA_COLOR = 'tab:blue'
 MODEL_COLOR = 'tab:red'
+MODEL_COLOR2 = 'tab:olive'
 
 
 def run_model_building(adj_matrix, nrn_table, model_name, model_order, **kwargs):
@@ -36,10 +37,14 @@ def run_model_building(adj_matrix, nrn_table, model_name, model_order, **kwargs)
         nrn_table = nrn_table.loc[sub_sel, :]
 
     # Set modelling functions
-    if model_order == 2:
+    if model_order == 2: # Distance-dependent
         fct_extract = extract_2nd_order
         fct_fit = build_2nd_order
         fct_plot = plot_2nd_order
+    elif model_order == 3: # Bipolar distance-dependent
+        fct_extract = extract_3rd_order
+        fct_fit = build_3rd_order
+        fct_plot = plot_3rd_order
     else:
         assert False, f'ERROR: Order-{model_order} model building not supported!'
 
@@ -104,6 +109,16 @@ def compute_dist_matrix(src_nrn_pos, tgt_nrn_pos):
     dist_mat[dist_mat == 0.0] = np.nan # Exclude autaptic connections
 
     return dist_mat
+
+
+def compute_bip_matrix(src_depths, tgt_depths):
+    """
+    Computes bipolar matrix between pairs of neurons based on depth difference delta_d:
+      POST-synaptic neuron below (delta_d < 0) or above (delta_d > 0) PRE-synaptic neuron
+    """
+    bip_mat = np.sign(-np.diff(np.meshgrid(src_depths, tgt_depths, indexing='ij'), axis=0)[0, :, :])
+
+    return bip_mat
 
 
 def extract_dependent_p_conn(adj_matrix, dep_matrices, dep_bins):
@@ -242,6 +257,140 @@ def plot_2nd_order(adj_matrix, nrn_table, model_name, p_conn_dist, count_conn, c
     plt.xlabel('Distance ($\\mu$m)')
     plt.ylabel('Count')
     plt.title(f'Distance-dependent connection counts (N = {nrn_table.shape[0]}x{nrn_table.shape[0]} cells)')
+    plt.legend()
+    plt.tight_layout()
+    if plot_dir is not None:
+        out_fn = os.path.abspath(os.path.join(plot_dir, model_name + '__data_counts.png'))
+        plt.savefig(out_fn)
+        print(f'INFO: Figure saved to {out_fn}')
+
+
+###################################################################################################
+# Generative models for circuit connectivity from [Gal et al. 2020]:
+#   3rd order (bipolar distance-dependent)
+###################################################################################################
+
+def extract_3rd_order(adj_matrix, nrn_table, bin_size_um=100, max_range_um=None, coord_names=None, depth_name=None, **_):    
+    """Extract distance-dependent connection probability (3rd order) from a sample of pairs of neurons."""
+
+    if coord_names is None:
+        coord_names = ['x', 'y', 'z'] # Default names of coordinatate system axes as in nrn_table
+    if depth_name is None:
+        depth_name = 'depth' # Default name of depth column in nrn_table
+
+    pos_table = nrn_table[coord_names].to_numpy()
+    depth_table = nrn_table[depth_name].to_numpy()
+
+    # Compute distance matrix
+    dist_mat = compute_dist_matrix(pos_table, pos_table)
+
+    # Compute bipolar matrix (post-synaptic neuron below (delta_d < 0) or above (delta_d > 0) pre-synaptic neuron)
+    bip_mat = compute_bip_matrix(depth_table, depth_table)
+
+    # Extract bipolar distance-dependent connection probabilities
+    if max_range_um is None:
+        max_range_um = np.nanmax(dist_mat)
+    num_dist_bins = np.ceil(max_range_um / bin_size_um).astype(int)
+    dist_bins = np.arange(0, num_dist_bins + 1) * bin_size_um
+    bip_bins = [np.nanmin(bip_mat), 0, np.nanmax(bip_mat)]
+
+    p_conn_dist_bip, count_conn, count_all = extract_dependent_p_conn(adj_matrix, [dist_mat, bip_mat], [dist_bins, bip_bins])
+
+    return {'p_conn_dist_bip': p_conn_dist_bip, 'count_conn': count_conn, 'count_all': count_all, 'dist_bins': dist_bins, 'bip_bins': bip_bins}
+
+
+def build_3rd_order(p_conn_dist_bip, dist_bins, **_):
+    """Build 3rd order model (bipolar exp. distance-dependent conn. prob.)."""
+    bin_offset = 0.5 * np.diff(dist_bins[:2])[0]
+
+    X = dist_bins[:-1][np.all(np.isfinite(p_conn_dist_bip), 1)] + bin_offset
+    y = p_conn_dist_bip[np.all(np.isfinite(p_conn_dist_bip), 1), :]
+
+    exp_model = lambda x, a, b: a * np.exp(-b * np.array(x))
+    (aN_opt, bN_opt), _ = opt.curve_fit(exp_model, X, y[:, 0], p0=[0.0, 0.0])
+    (aP_opt, bP_opt), _ = opt.curve_fit(exp_model, X, y[:, 1], p0=[0.0, 0.0])
+
+    print(f'BIPOLAR MODEL FIT: f(x, dz) = {aN_opt:.3f} * exp(-{bN_opt:.3f} * x) if dz < 0')
+    print(f'                              {aP_opt:.3f} * exp(-{bP_opt:.3f} * x) if dz > 0')
+    print('                              AVERAGE OF BOTH MODELS  if dz == 0')
+
+    model = 'np.select([np.array(dz) < 0, np.array(dz) > 0, np.array(dz) == 0], [aN_opt * np.exp(-bN_opt * np.array(d)), aP_opt * np.exp(-bP_opt * np.array(d)), 0.5 * (aN_opt * np.exp(-bN_opt * np.array(d)) + aP_opt * np.exp(-bP_opt * np.array(d)))])'
+    model_inputs = ['d', 'dz']
+    model_params = {'aN_opt': aN_opt, 'bN_opt': bN_opt, 'aP_opt': aP_opt, 'bP_opt': bP_opt}
+
+    return {'model': model, 'model_inputs': model_inputs, 'model_params': model_params}
+
+
+def plot_3rd_order(adj_matrix, nrn_table, model_name, p_conn_dist_bip, count_conn, count_all, dist_bins, model, model_inputs, model_params, plot_dir=None, **_):
+    """Visualize data vs. model (3rd order)."""
+    if plot_dir is not None:
+        if not os.path.exists(plot_dir):
+            os.makedirs(plot_dir)
+
+    bin_offset = 0.5 * np.diff(dist_bins[:2])[0]
+    dist_model = np.linspace(dist_bins[0], dist_bins[-1], 100)
+
+    model_strN = f'{model_params["aN_opt"]:.3f} * exp(-{model_params["bN_opt"]:.3f} * x)'
+    model_strP = f'{model_params["aP_opt"]:.3f} * exp(-{model_params["bP_opt"]:.3f} * x)'
+    model_fct = get_model_function(model, model_inputs, model_params)
+
+    plt.figure(figsize=(12, 4), dpi=300)
+
+    # Data vs. model
+    plt.subplot(1, 2, 1)
+    bip_dist = np.concatenate((-dist_bins[:-1][::-1] - bin_offset, [0.0], dist_bins[:-1] + bin_offset))
+    bip_data = np.concatenate((p_conn_dist_bip[::-1, 0], [np.nan], p_conn_dist_bip[:, 1]))
+    all_bins = np.concatenate((-dist_bins[1:][::-1], [0.0], dist_bins[1:]))
+    bin_data = np.concatenate((p_conn_dist_bip[::-1, 0], p_conn_dist_bip[:, 1]))
+    plt.step(all_bins, np.hstack([bin_data[0], bin_data]), color=DATA_COLOR, label=f'Data: N = {nrn_table.shape[0]}x{nrn_table.shape[0]} cells')
+    plt.plot(bip_dist, bip_data, '.', color=DATA_COLOR)
+    plt.plot(-dist_model, model_fct(dist_model, np.sign(-dist_model)), '--', color=MODEL_COLOR, label='Model: ' + model_strN)
+    plt.plot(dist_model, model_fct(dist_model, np.sign(dist_model)), '--', color=MODEL_COLOR2, label='Model: ' + model_strP)
+    plt.grid()
+    plt.xlabel('sign($\\Delta$z) * Distance [$\\mu$m]')
+    plt.ylabel('Conn. prob.')
+    plt.title('Data vs. model fit')
+    plt.legend(loc='upper left', fontsize=8)
+
+    # 2D connection probability (model)
+    plt.subplot(1, 2, 2)
+    plot_range = 500 # (um)
+    r_markers = [200, 400] # (um)
+    dx = np.linspace(-plot_range, plot_range, 201)
+    dz = np.linspace(plot_range, -plot_range, 201)
+    xv, zv = np.meshgrid(dx, dz)
+    vdist = np.sqrt(xv**2 + zv**2)
+    pdist = model_fct(vdist, np.sign(zv))
+    plt.imshow(pdist, interpolation='bilinear', extent=(-plot_range, plot_range, -plot_range, plot_range), cmap=PROB_CMAP, vmin=0.0)
+    plt.plot(plt.xlim(), np.zeros(2), 'w', linewidth=0.5)
+    for r in r_markers:
+        plt.gca().add_patch(plt.Circle((0, 0), r, edgecolor='w', linestyle='--', fill=False))
+        plt.text(0, r, f'{r} $\\mu$m', color='w', ha='center', va='bottom')
+    plt.xticks([])
+    plt.yticks([])
+    plt.xlabel('$\\Delta$x')
+    plt.ylabel('$\\Delta$z')
+    plt.title('2D model')
+    plt.colorbar(label='Conn. prob.')
+
+    plt.suptitle(f'Bipolar distance-dependent connection probability model (3rd order)')
+    plt.tight_layout()
+    if plot_dir is not None:
+        out_fn = os.path.abspath(os.path.join(plot_dir, model_name + '__data_vs_model.png'))
+        plt.savefig(out_fn)
+        print(f'INFO: Figure saved to {out_fn}')
+
+    # Data counts
+    bip_count = np.concatenate((count_conn[::-1, 0], [np.nan], count_conn[:, 1]))
+    bip_count_all = np.concatenate((count_all[::-1, 0], [np.nan], count_all[:, 1]))
+    plt.figure(figsize=(12, 4), dpi=300)
+    plt.bar(bip_dist, bip_count_all, width=2.0 * bin_offset, edgecolor='k', label='All pair count')
+    plt.bar(bip_dist, bip_count, width=1.5 * bin_offset, label='Connection count')
+    plt.gca().set_yscale('log')
+    plt.grid()
+    plt.xlabel('sign($\\Delta$z) * Distance [$\\mu$m]')
+    plt.ylabel('Count')
+    plt.title(f'Bipolar distance-dependent connection counts (N = {nrn_table.shape[0]}x{nrn_table.shape[0]} cells)')
     plt.legend()
     plt.tight_layout()
     if plot_dir is not None:

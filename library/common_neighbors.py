d1011c20fa902caafea592e438ddcac24438aefd
Michael Reimann <michael@reimann.science>
2021-12-15 17:11:58 +0100

    First set of common neighbor analyses: Only number of CN, not the connectivity bias.

---
+++ library/common_neighbors.py
@@ -0,0 +1,46 @@
+import numpy
+import pandas
+
+
+def __make_expected_distribution_model_first_order__(adj, direction="efferent"):
+    from scipy.stats import hypergeom
+    if direction == "efferent":
+        N = adj.sum(axis=1).mean()
+        M = adj.shape[1]
+    elif direction == "afferent":
+        N = adj.sum(axis=0).mean()
+        M = adj.shape[0]
+    else:
+        raise ValueError("Unknown direction: {0}".format(direction))
+    expected = hypergeom(M, N, N)
+    return expected
+
+
+def distribution_number_of_common_neighbors(adj, neuron_properties=None, direction="efferent"):
+    adj = adj.tocsc().astype(int)
+    if direction == "efferent":
+        cn = adj * adj.transpose()
+    elif direction == "afferent":
+        cn = adj.transpose() * adj
+    else:
+        raise ValueError("Unknown direction: {0}".format(direction))
+    cn = numpy.array(cn.todense())
+    cn = cn[numpy.triu_indices_from(cn, 1)]
+    bins = numpy.arange(0, cn.max() + 2)
+    return pandas.Series(numpy.histogram(cn, bins=bins)[0], index=bins[:-1])
+
+
+def normalized_distribution_of_common_neighbors(adj, neuron_properties=None, direction="efferent"):
+    data = distribution_number_of_common_neighbors(adj, neuron_properties, direction=direction)
+    expected = __make_expected_distribution_model_first_order__(adj, direction=direction)
+    expected = expected.pmf(data.index) * data.sum()
+    expected = pandas.Series(expected, index=data.index)
+    return (data - expected) / (data + expected)
+
+
+def overexpression_of_common_neighbors(adj, neuron_properties=None, direction="efferent"):
+    data = distribution_number_of_common_neighbors(adj, neuron_properties, direction=direction)
+    data_mean = (data.index.values * data.values).sum() / data.values.sum()
+    ctrl = __make_expected_distribution_model_first_order__(adj, direction=direction)
+    ctrl_mean = ctrl.mean()
+    return (data_mean - ctrl_mean) / (data + ctrl_mean)

828ab532b34347a3e20460760bcacffec8f5910a
Guillaume Tauzin <gtauzin@protonmail.com>
2022-12-14 21:46:17 +0100

    Revert "[WIP - DO NOT MERGE] Packaging with poetry + restructuration"

--- src/connalysis/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Functions for network analysis of graphs coming brain models."""
-
-__all__ = [
-    "modelling",
-]
--- tests/test_imports.py
+++
@@ -1,4 +0,0 @@
-def test_imports():
-    import connalysis
-    import generate_model
-    from generate_model import ER

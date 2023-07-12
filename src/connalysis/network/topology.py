d82d1717fccbfa0ebc050ed21fd25f30430a4f35
Jason Smith <jasonsmith.bath@gmail.com>
2023-07-12 14:29:57 +0100

    Added function for simplicial rich club v4

--- src/connalysis/network/topology.py
+++ src/connalysis/network/topology.py
@@ -1564,5 +1564,5 @@ def betti_curve(B,D):
 def simplicial_rich_club_curve(M, maximal=False, sparse_bin_set=False):
     import pyflagsercount
     from .classic import efficient_rich_club_curve
-    vertex_par = pd.DataFrame(pyflagsercount.flagser_count(M, max_simplices=maximal, containment=True)['contain_counts']).replace(np.nan,0)
-    return pd.DataFrame([efficient_rich_club_curve(M, pre_calculated_richness=vertex_par[i]) for i in range(vertex_par.shape[1])]).transpose()
+    vertex_par = pd.DataFrame(pyflagsercount.flagser_count(M, max_simplices=maximal, containment=True)['contain_counts']).replace(np.nan,0).astype(int)
+    return pd.DataFrame([efficient_rich_club_curve(M, pre_calculated_richness=vertex_par[i]) for i in range(vertex_par.shape[1])]).transpose().dropna(how='all')
